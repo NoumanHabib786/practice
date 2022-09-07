966a450c443015570a024caf82acd3e708a66964
Nouman Mughal <noumanhabib0348@gmail.com>
2022-09-07 13:56:57 +0500

    tset

---
+++ index.js
@@ -0,0 +1,73 @@
+// var name="<h1> Nouman </h1>"  ;
+// var  f_name="<h1>habib</h1>" ;
+// var  Roll_no="<h1>33</h1> ";
+// var red_color ="<h1><font color ='red'>i am red now</font></h1>";
+// var click_me= "<a href='https:\\www.geeksforgeeks.org'>Clicl me to go a beautiful learining skills </a> "
+// var para = "<p>";
+
+
+// document.write( name + f_name);
+// document.write(Roll_no);
+// document.write(para +red_color);
+// document.write(click_me);
+
+// function first (var1, msg, var2 , name , f_name)
+// {
+//     //javascript code
+//     var1 = 2;
+//     var2 = 3;
+//     msg = var1%var2;
+//     if( var1 < var2)
+//     {
+//         var i ;
+//         for ( i=0 ; i<5 ; i++ )
+//         {
+
+//             for(j=i ; j<5 ; j++)
+//             {
+            
+//                 name = "Nouman";
+//                 f_name = "<b>Habib<br></b>"
+//                 document.write("My Name is "+ name +"And My father Name is"+f_name );
+//             }
+//         }
+    
+//     }
+//     else
+//     {
+//        alert(msg);
+//     }
+// }
+// first();
+
+// var count= 1;
+// while(count<6)
+// {
+//     document.write("Loop work<br>");
+//     count+=1;
+
+
+// }
+function hey(msg,msg2="why did you change")
+{
+     msg="Thanks for Submitting";
+     
+    alert(msg);
+    alert(msg2);
+
+
+   
+}
+function by ()
+{
+   for(i=0 ; i<5 ; i++)
+   {
+    document.write(i+"\t");
+    
+    for(j=1 ; j <i ; j++)
+    {
+        document.write("value of J is"+j+"<br>");
+    }
+   }
+}
+
